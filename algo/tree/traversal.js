89008a448bc69630b375c816848f66dbccc34825
mingyec <mingyec@qq.com>
2020-07-18 20:06:49 +0800

    二叉树：遍历

---
+++ algo/tree/traversal.js
@@ -0,0 +1,153 @@
+// 二叉树的遍历
+
+/**
+ * 树节点
+ * @param {*} val 
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 前序遍历
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    const arr = [];
+
+    const fn = node => {
+        if (!node.val) {
+            return
+        }
+        arr.push(node.val);
+        fn(node.left);
+        fn(node.right)
+    }
+    fn(root)
+    return arr
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 中序遍历
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    const arr = [];
+    const fn = r => {
+        if (!r) {
+            return
+        }
+        fn(r.left);
+        arr.push(r.val)
+        fn(r.right);
+    }
+    fn(root)
+    return arr
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 后序遍历
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+    const arr = []
+    const fn = r => {
+        if (r && r.val) {
+            fn(r.left);
+            fn(r.right);
+            arr.push(r.val)
+        }
+    }
+    fn(root)
+    return arr;
+};
+
+// 迭代：颜色标记法
+postorderTraversal = function (root) {
+    const arr = [];
+    if (!root) return arr;
+
+    const stack = [];
+    stack.push({
+        color: 'white',
+        node: root
+    })
+
+    while (stack.length > 0) {
+        const { color, node } = stack.pop();
+        if (color === 'gray') {
+            arr.push(node.val);
+        } else {
+            // 压入根
+            stack.push({
+                color: 'gray',
+                node: node
+            })
+            // 压入右
+            node.right && stack.push({
+                color: 'white',
+                node: node.right
+            })
+            // 压入左
+            node.left && stack.push({
+                color: 'white',
+                node: node.left
+            })
+        }
+    }
+    return arr;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 层序遍历
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    const queue = [root];
+    const arr = []
+    while (queue.length > 0) {
+        arr.push([]);
+        let i = 1;
+        const qSize = queue.length;
+        while (i <= qSize) {
+            const node = queue.shift()
+            arr[arr.length - 1].push(node.val);
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+            i++;
+        }
+    }
+};
