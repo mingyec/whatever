3c2d0a925aa7acd8bcc6ce26bf8c1c0af2e37423
mingyec <mingyec@qq.com>
2020-08-06 00:05:21 +0800

    树：翻转二叉树

---
+++ algo/tree/invertTree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (root === null) return null;
+    // 交换左右节点
+    [root.left, root.right] = [root.right, root.left];
+    invertTree(root.left);
+    invertTree(root.right);
+};
