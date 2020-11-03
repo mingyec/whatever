af6530a291cd3d2133dcb08d1a7113e488752a4f
mingyec <mingyec@qq.com>
2020-11-03 22:58:18 +0800

    算法：移除元素

---
+++ algo/array/removeElement.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    if (!nums.length) return 0;
+    let slow = 0,
+        fast = 0;
+    while (fast < nums.length) {
+        if (nums[fast] !== val) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
+};
